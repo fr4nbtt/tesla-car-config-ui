c3556b27bcce23225beedb7bb1f6932ae0a46482
fr4nbtt <batet.francisco@gmail.com>
2024-12-23 20:06:37 -0300

    feat: implement color selection in JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const topBar = document.querySelector("#top-bar");
+const exteriorColorSection = document.querySelector("#exterior-buttons");
+const interiorColorSection = document.querySelector("#interior-buttons");
+const exteriorImage = document.querySelector("#exterior-image");
+const interiorImage = document.querySelector("#interior-image");
 
 /* Handle Top Bar On Scroll */
 const handleScroll = () => {
@@ -9,5 +13,24 @@ const handleScroll = () => {
   topBar.classList.toggle("hidden-bar", !atTop);
 };
 
+/* Handle Color Selection */
+const handleColorButtonClick = (e) => {
+  let button;
+
+  if (e.target.tagName === "IMG") {
+    button = e.target.closest("button");
+  } else if (e.target.tagName === "BUTTON") {
+    button = e.target;
+  }
+
+  if (button) {
+    const buttons = e.currentTarget.querySelectorAll("button");
+    buttons.forEach((btn) => btn.classList.remove("btn-selected"));
+    button.classList.add("btn-selected");
+  }
+};
+
 /* Event listeners */
 window.addEventListener("scroll", () => requestAnimationFrame(handleScroll));
+exteriorColorSection.addEventListener("click", handleColorButtonClick);
+interiorColorSection.addEventListener("click", handleColorButtonClick);
